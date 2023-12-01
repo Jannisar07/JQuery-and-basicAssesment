c31a48ff105c637ada17cfc4e4560d3bc543092a
Jannisar07 <132677668+Jannisar07@users.noreply.github.com>
2023-12-01 15:58:50 +0530

    Delete scriptForAssessment.js

--- scriptForAssessment.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#bt").click(function(){
-        // $("#bx").toggle("display","block")
-        $("#bx").toggle()
-        $("#bx a").css("display","block")
-
-        $("#bt").css("color","red")
-    })
-})
